71a22ce904e92df41ff70c5d5178fce55a495d77
Davide Di Stefano <davide.distefano@arancia-ict.it>
2019-05-16 14:11:06 +0200

    removed symbolik link to docker compose

--- setupLibrary.sh
+++ setupLibrary.sh
@@ -169,7 +169,6 @@ function docker() {
 function dockerCompose() {
     sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
     sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 }
 
